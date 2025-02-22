33aba9e0fc8e06005b71dde44b7047e6235e5add
Rafael Freitas <rafael.freitasfa@gmail.com>
2025-02-22 16:26:48 -0300

    Resolvendo erro no Redis

--- src/redis/client.ts
+++ src/redis/client.ts
@@ -1,4 +1,5 @@
-import { Redis } from 'ioredis'
-import { env } from '../env'
+import { Redis } from 'ioredis';
+import { env } from '../env';
 
-export const redis = new Redis(env.REDIS_URL)
+// biome-ignore lint/style/useTemplate: <explanation>
+export const redis = new Redis(env.REDIS_URL + '?family=0');
